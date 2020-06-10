a3808c771904a6787650b4a9d1216c8c3f2ae21b
Sofija <sofija.nikitina@inbox.lv>
2020-06-10 23:05:32 +0300

    Updates

---
+++ precache-manifest.f8f6dbcb5cd55a14b820f273d1cc474e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc677f1347e91596af892ff13371ef20",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "99ea903365d742df5f0f",
+    "url": "/to-do-list/static/css/main.37cf6eed.chunk.css"
+  },
+  {
+    "revision": "e3a3706ccc2b2c027518",
+    "url": "/to-do-list/static/js/2.7ca73ff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.7ca73ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ea903365d742df5f0f",
+    "url": "/to-do-list/static/js/main.258bc913.chunk.js"
+  },
+  {
+    "revision": "928c55de99524ec4aa26",
+    "url": "/to-do-list/static/js/runtime-main.f99e8931.js"
+  }
+]);
